1856c6f2c96d02b85f8950026492d15c0cca7454
Exohacked <126220006+Exohacked@users.noreply.github.com>
2025-01-11 12:00:45 +0100

    changes

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 //import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { ArrowRight, Star, Users, Printer, ShoppingBag } from 'lucide-react';
