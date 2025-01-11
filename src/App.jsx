e6f26e05719ec9fa2883cd706067fbd7382c6d94
Exohacked <126220006+Exohacked@users.noreply.github.com>
2025-01-11 15:29:07 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import MainPage from "./Pages/MainPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./Components/Navbar";
 import JoinUs from "./Pages/JoinUs";
 
 function App() {
